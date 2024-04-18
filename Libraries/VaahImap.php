4a8fb3f97e8910e7daa1cbc3bd75f9dce29a700d
Pradeep <webreinvent@gmail.com>
2024-04-18 17:51:53 +0530

    updated: a method

--- Libraries/VaahImap.php
+++ Libraries/VaahImap.php
@@ -364,5 +364,10 @@ class VaahImap{
         return $this->imap->markMailAsUnread($uid);
     }
     //----------------------------------------------------------
+    function getMailAttachments($mail_uid){
+        $this->connect();
+        $mail = $this->imap->getMail($mail_uid);
+        return $mail->getAttachments();
+    }
     //----------------------------------------------------------
 }
